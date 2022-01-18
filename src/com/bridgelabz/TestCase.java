4e251c64903375d46a65d93b82e34fe0984cc05c
laxman-taur9 <laxmantaur9@gmail.com>
2022-01-18 15:11:44 +0530

    Modified: using Generic method

--- src/com/bridgelabz/TestCase.java
+++ src/com/bridgelabz/TestCase.java
@@ -2,25 +2,14 @@ package com.bridgelabz;
 
 public class TestCase {
 
-	public static void toPrint(Integer[] inputArray) {
-		for(int element : inputArray) {
+	public static <E> void toPrint(E[] inputArray) {
+		for(E element : inputArray) {
 			System.out.printf("%s",element);
 		}
 		System.out.println();
 	}
 
-	public static void toPrint(Double[] inputArray) {
-		for(double element : inputArray) {
-			System.out.printf("%s",element);
-		}
-		System.out.println();
-	}
-	public static void toPrint(Character[] inputArray) {
-		for(char element : inputArray) {
-			System.out.printf("%s",element);
-		}
-		System.out.println();
-	}
+
 	public static void main(String[] args) {
 		Integer[] intArray = { 1, 2, 3, 4, 5};
 		Double[] doubleArray = {1.1, 2.2, 3.3, 4.4 };
